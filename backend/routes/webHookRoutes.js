fe505cefed044a81170287fee5425b52e4756ad5
beingpourush <pourushgoyal2005@gmail.com>
2026-07-09 23:19:39 +0530

    Fix webHookController import

--- backend/routes/webHookRoutes.js
+++ backend/routes/webHookRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { clerkWebhook } = require("../controllers/webhookController");
+const { clerkWebhook } = require("../controllers/webHookController");
 
 router.post("/clerk", clerkWebhook);
 
